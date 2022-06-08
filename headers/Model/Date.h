ff44b8aef08e1bf7bd52a97856ba074de1b6091c
beatriz <1211081@isep.ipp.pt>
2022-06-08 17:42:49 +0100

    corrigi alguns erros

--- headers/Model/Date.h
+++ headers/Model/Date.h
@@ -1,17 +1,17 @@
-#include <iostream>
-#include <string>
 
 
 #ifndef FSOFT2022_1A_5_DATE_H
 #define FSOFT2022_1A_5_DATE_H
+
+#include <iostream>
+#include <string>
+
 
 class Date{
 private:
     int day;
     int month;
     int year;
-    bool IsLeapYear(int year);
-    bool IsValid(int day, int month,int year);
 
 public:
     Date()
@@ -20,9 +20,12 @@ public:
     void AddDate (int day, int month, int year);
     void GetDate (int& day, int& month, int& year) const;
 
-    bool operator == (consta Date& obj) const;
-    bool operator < (consta Date& obj) const;
-    bool operator > (consta Date& obj) const;
+    bool IsLeapYear(int year);
+    bool IsValid(int day, int month,int year);
+
+    bool operator == (const Date& obj) const;
+    bool operator <  (const Date& obj) const;
+    bool operator >  (const Date& obj) const;
 };
 
 
