2953e47989c21435923e1c3b3d5b9f82ac669428
beatriz <1211081@isep.ipp.pt>
2022-06-09 17:17:56 +0100

    update- person.cpp incompleto

--- source/Model/person.cpp
+++ source/Model/person.cpp
@@ -2,37 +2,24 @@
 // Created by Ana Rita Maia Barbosa da Silva on 05/06/2022.
 //
 
+
 #include "Person.h"
-#include "invalidDataException.h"
 
-bool Person::isNameValid(const string& name){
-    if(name.lenght() < 2){ //se o nome tiver menos de 2 letras, retorna false, senao retorna true
-        return false;
-    }else{
-        return true;
-    }
-}
-
-//o código está igual ao do professor...
-Person::Person(const string& name){
-    setName(name);
-}
-Person::Person(const Person& person){
-    setName(person.name);
+Person::Person(string name) {
+    this->name = name;
 }
 Person::~Person(){
+    this->name = "none";
+}
+string Person::getName() const {
+    return this->name;
 }
 
-const string&  Person::getName() const {
-    return name;
+void Person::setName(string name){
+    this->name = name;
+    
 }
 
-void  Person::setName(const string &name) {
-    if(isNameValid(name)){
-        this->name = name;
-    }else{
 
-        string msg = "Person: " + name;
-        throw invalidDataException(msg);
-    }
-}
+
+
