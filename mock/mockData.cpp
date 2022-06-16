10c8c6f2a49698416a3bd1706763c8687655e504
AnaRita03 <1211152@isep.ipp.pt>
2022-06-16 13:28:21 +0100

    update

--- mock/mockData.cpp
+++ mock/mockData.cpp
@@ -3,20 +3,33 @@
 //
 
 #include "mockData.h"
-#include "duplicatedDataException.h"
+#include "/Users/anarita/Desktop/FSOFT3/fsoft2022_1A_5/headers/Exceptions/duplicatedDataException.h"
 
-
-
-
+/*
 void mockData::insertClientFile(ClientFileContainer& container){
-    for (int i = 0; i < name_client.size(); ++i){
+    for (size_t i = 0; i < name_client.size(); ++i){
+        ClientFile obj (name_clientFile[i], email_clientFile[i], phone_number_clientFile[i]);
+        try{
+            container.add(obj);
+        }catch(DuplicatedDataException& e){
 
+        }
+    }
+}
+*/
+
+void mockData::insertClientsFile(ClientFileContainer& container){
+    for(size_t i = 0; i < name_clientFile.size(); i++){
+        ClientFile obj (name_clientFile[i], phone_number_clientFile[i], email_clientFile[i]);
+        container.add(obj);
     }
 }
 
-//?
 void mockData::insertEmployee(EmployeeContainer& container){
-    for (int i = 0; i < name_employee.size(); ++i){
-
+    for (size_t i = 0; i < name_employee.size(); ++i){
+        Employee obj (name_employee[i], specialty_employee[i], store[i]);
+        container.add(obj);
     }
 }
+
+
