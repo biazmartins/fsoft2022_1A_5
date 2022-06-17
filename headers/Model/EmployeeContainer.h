b989b7fb0ea07d3464e650b1c0c569f311cc8311
Beatriz Martins <1211081@isep.ipp.pt>
2022-06-17 15:50:47 +0100

    update - EmployeeContainer.h
    EmployeeContainer.h completo

--- headers/Model/EmployeeContainer.h
+++ headers/Model/EmployeeContainer.h
@@ -1,8 +1,23 @@
-//
-// Created by beatr on 09/06/2022.
-//
-
 #ifndef FSOFT2022_1A_5_EMPLOYEECONTAINER_H
 #define FSOFT2022_1A_5_EMPLOYEECONTAINER_H
+
+
+#include <list>
+#include "employee.h"
+#include "Date.h"
+
+class EmployeeContainer{
+private:
+    list<Employee> employee;
+    list<Employee>::iterator search(string store, string specialty);
+
+public:
+    list<Employee> getAll();
+    ClientFile* get(sting store, string specialty);
+
+
+    void add(Employee obj);
+    void remove((sting store, string specialty);
+    void update(sting store, string specialty);
 
 #endif //FSOFT2022_1A_5_EMPLOYEECONTAINER_H
