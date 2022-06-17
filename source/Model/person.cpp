6e94df8eeddd3f0feb25b014ace59e295a0080f1
Beatriz Martins <1211081@isep.ipp.pt>
2022-06-17 14:31:53 +0100

    update - person.cpp
    person.cpp completo

--- source/Model/person.cpp
+++ source/Model/person.cpp
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-//
-// Created by Ana Rita Maia Barbosa da Silva on 05/06/2022.
-//
 
-
-=======
->>>>>>> 9f411bb9b2c14a4685e0cfb70f45812eb19269ba
 #include "Person.h"
 
 Person::Person(string name) {
@@ -20,15 +13,5 @@ string Person::getName() const {
 
 void Person::setName(string name){
     this->name = name;
-<<<<<<< HEAD
-
 }
 
-
-
-
-=======
-
-    
-    
->>>>>>> 9f411bb9b2c14a4685e0cfb70f45812eb19269ba
