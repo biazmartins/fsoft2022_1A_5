aa3f4f466c65714a9fc23bdf46ea1d4edc14cccc
Beatriz Martins <1211081@isep.ipp.pt>
2022-06-17 12:29:18 +0100

    update
    update- ClientFileContainer.h completo

--- headers/Model/ClientFileContainer.h
+++ headers/Model/ClientFileContainer.h
@@ -3,19 +3,22 @@
 
 #include <list>
 #include "ClientFile.h"
+#include "Date.h"
 
 class ClientFileContainer{
 private:
     list<ClientFile> clientFile;
-    list<ClientFile>::iterator search(string name);
     list<ClientFile>::iterator search(int phone_number);
-    list<ClientFile>::iterator search(const string& email);
 
 public:
     list<ClientFile> getAll();
-    ClientFile* get(string name);
     ClientFile* get(int phone_number);
-    ClientFile* get(const string& email);
+
+
+    void add(ClientFile obj);
+    void remove(int phone_number);
+    void update(int phone_number, Date date);
+
 
 };
 
