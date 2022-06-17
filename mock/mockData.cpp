776601eb926f92ce660d1e55adffd8aeee1274f4
AnaRita03 <1211152@isep.ipp.pt>
2022-06-17 11:59:13 +0100

    Update mockData.cpp
    mockData update

--- mock/mockData.cpp
+++ mock/mockData.cpp
@@ -25,11 +25,38 @@ void mockData::insertClientsFile(ClientFileContainer& container){
     }
 }
 
-void mockData::insertEmployee(EmployeeContainer& container){
+void mockData::insertEmployees(EmployeeContainer& container){
     for (size_t i = 0; i < name_employee.size(); ++i){
         Employee obj (name_employee[i], specialty_employee[i], store[i]);
         container.add(obj);
     }
 }
 
+//?
+void mockData::insertWorkshops(WorkshopContainer& container){
+    for(size_t i = 0; 1 < this-> n_workshops.size(); i++){
 
+    }
+}
+
+Workshop * mockData::getRandomWorkshop(WorkshopContainer &container){
+    list<Workshop> listWorkshop = workshops.getAll();
+    list<Workshop>::iterator it = listClass.begin();
+    int is = rand() % listWorkshop.size();
+    for (int i = 0; i < is; ++i) {
+        it++;
+    }
+    //it = it + is
+    string name = it -> getInitials();
+    Workshop * ptr = workshops.get(name);
+    return ptr;
+}
+
+void mockData::generateData(MakeupStore *makeupStore){
+    srand (time(NULL));
+    insertClientsFile(makeupStore.getClientFileContainer());
+    insertEmployees(makeupStore.getEmployeeContainer());
+    insertWorkshops(makeupStore.getWorkshopContainer());
+}
+
+//coment
