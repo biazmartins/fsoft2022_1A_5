c8a53a8507039977ed978ae863287c5ee80bf3e6
AnaRita03 <1211152@isep.ipp.pt>
2022-06-19 12:00:16 +0100

    Update WorkshopView.cpp
    workshoView.cpp

--- source/Views/WorkshopView.cpp
+++ source/Views/WorkshopView.cpp
@@ -21,17 +21,17 @@ Workshop WorkshopView :: getWorkshop(EmployeeContainer & employees){
 
         } catch (invalidDataException &e) {
             flag = true;
-        };
+        }
     while (flag == true);
     return workshop;
 }
 
 void WorkshopView :: printWorkshop(Workshop *workshop) {
     Employee *employee = Workshop->getEmployee;
-    cout << "\t" << employee->getInitials() << ":" << employee->getDesignation() << endl;
+    cout << "\t" << employee->getName() << ":" << employee->getStore() << ":" << employee->getSpecialty() << endl;
 }
 void WorkshopView :: printWorkshop(Employee *employee, list<Workshop> Workshop) {
-    cout << employee->getInitials() << ":" << employee->getName() << endl;
+    cout << employee->getName() << ":" << employee->getName() << endl;
     for(list<Workshop>::iterator it=workshops.begin(); it!=workshops.end(); ++it){
         printWorkshop(*it);
     }
